8faa4b3c10390abf21d8912d5ab6f54b3b0b6599
Josh <jthalappillil7@gmail.com>
2023-08-29 21:02:07 +1000

    Completion of more exercises for Java Course. Begun conditional operations exercises.

--- part01-Part01_28.Positivity/src/main/java/Positivity.java
+++ part01-Part01_28.Positivity/src/main/java/Positivity.java
@@ -5,7 +5,8 @@ public class Positivity {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("");
 
-        // Write your program here
+
     }
 }
