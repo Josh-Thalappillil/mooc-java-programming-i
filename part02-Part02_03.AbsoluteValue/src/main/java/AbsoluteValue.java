abc8d415e0d91a078204fb8cd238a68129b5abb4
Josh <jthalappillil7@gmail.com>
2023-08-31 01:38:25 +1000

    Completion of Part 2 conditional statements

--- part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -6,5 +6,12 @@ public class AbsoluteValue {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int num = Integer.valueOf(scanner.nextLine());
+
+        if (num < 0){
+            System.out.println(Math.abs(num));
+        } else {
+            System.out.println(num);
+        }
     }
 }
