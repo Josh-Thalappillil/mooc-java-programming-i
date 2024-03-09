27743a2fde0757f1ffa0e9a543730598f0614454
Josh <jthalappillil7@gmail.com>
2024-03-09 21:28:02 +1100

    Update OnTheList.java
    completing mooc java exercises part 3

--- part03-Part03_14.OnTheList/src/main/java/OnTheList.java
+++ part03-Part03_14.OnTheList/src/main/java/OnTheList.java
@@ -13,8 +13,13 @@ public class OnTheList {
             if (input.equals("")) {
                 break;
             }
-
             list.add(input);
+        }
+        String input2 = scanner.nextLine();
+        if (list.contains(input2)){
+            System.out.println(input2 + " was found!");
+        } else {
+            System.out.println(input2 + " was not found!");
         }
 
     }
