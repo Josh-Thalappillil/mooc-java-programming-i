0297901d3ec6378f21e969cc0d7738de0eb67fb8
Josh <jthalappillil7@gmail.com>
2024-03-26 12:45:56 +1100

    Completion of OOP Part 4-1

--- part04-Part04_13.Agent/src/main/java/Agent.java
+++ part04-Part04_13.Agent/src/main/java/Agent.java
@@ -1,17 +1,17 @@
 
 
 public class Agent {
-
     private String firstName;
     private String lastName;
-    
-    public Agent(String initFirstName, String initLastName) {
-        this.firstName = initFirstName;
-        this.lastName = initLastName;
+
+    public Agent(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
-    
-    public void print() {
-    System.out.println("My name is " + this.lastName + ", " + this.firstName + " " + this.lastName);
+
+    @Override
+    public String toString() {
+        return "My name is " + lastName + ", " + firstName + " " + lastName;
     }
-    
 }
+
--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -1,16 +1,31 @@
-
 import java.util.Scanner;
 
 public class MainProgram {
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // you can write test code here
-        // however, remove all unnecessary code when doing the final parts of the exercise
+        Statistics totalSum = new Statistics();
+        Statistics evenSum = new Statistics();
+        Statistics oddSum = new Statistics();
 
-        // In order for the tests to work, the objects must be created in the
-        // correct order in the main program. First the object that tracks the total
-        // sum, secondly the object that tracks the sum of even numbers, 
-        // and lastly the one that tracks the sum of odd numbers!
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter numbers:");
+
+        while (true) {
+            int number = Integer.parseInt(scanner.nextLine());
+            if (number == -1) {
+                break;
+            }
+
+            totalSum.addNumber(number);
+            if (number % 2 == 0) {
+                evenSum.addNumber(number);
+            } else {
+                oddSum.addNumber(number);
+            }
+        }
+
+        System.out.println("Sum: " + totalSum.sum());
+        System.out.println("Sum of even numbers: " + evenSum.sum());
+        System.out.println("Sum of odd numbers: " + oddSum.sum());
     }
 }
+
