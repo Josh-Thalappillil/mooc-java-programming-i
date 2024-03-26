ae0b9a75e2c93df9c96d6378f70c59e531cae862
Josh <jthalappillil7@gmail.com>
2024-03-26 14:44:00 +1100

    Part 4 exercise 22 uploaded

--- part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -1,10 +1,20 @@
-
 import java.util.Scanner;
 
 public class NumberOfStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        String input;
 
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+            count++;
+        }
+
+        System.out.println(count);
     }
 }
--- part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -1,10 +1,25 @@
-
 import java.util.Scanner;
 
 public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String input;
 
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+            try {
+                int number = Integer.parseInt(input);
+                int cube = number * number * number;
+                System.out.println(cube);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number or 'end' to quit.");
+            }
+        }
+
+        scanner.close();
     }
 }
